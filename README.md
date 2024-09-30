e66e2a8e325358d8cf98bc63d924d3e05875c742
LightningLaser8 <LightningLaser8@users.noreply.github.com>
2024-09-30 18:13:49 +0100

    Update to (and past) v0.1.0-alpha

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-[![Any ISL version supported](https://img.shields.io/badge/ISL-any-00e3ff "Any ISL version supported")](https://github.com/LightningLaser8/ISL)
-# multisl
-MultISL is an ISL extension that allows ISL programs to utilise multiple disposable interpreters ("threads"), each one running entirely separately.
-Each interpreter has the same options as the original interpreter, and run asynchronously.
-
+[![Any ISL version after v0.1.0-alpha supported](https://img.shields.io/badge/ISL-after_v0.1.0--alpha-00e3ff "Any ISL version after v0.1.0-alpha supported")](https://github.com/LightningLaser8/ISL)
+# MultISL
+MultISL is an ISL extension that allows ISL programs to utilise multiple disposable interpreters ("threads"), each one running entirely separately.  
+Each interpreter has the same options as the original interpreter, and run asynchronously.  
+*Note: Does not actually run in separate system threads, but in async loops.*
+## Keywords: `thread`
+There's only one keyword added by this extension: `thread`  
+It performs all available operations on threads.  
+### Syntax
+```isl
+<label> thread <thread> [...code]
+```
+Performs an operation determined by the label on the specified thread.
+### Labels
+`create`: Makes a new thread.  
+`delete`: Removes an existing thread.  
+`add-to`: Adds some ISL to the thread's interpreter.  
+`in`: Executes some ISL in the thread's interpreter directly.  
+`start`: Starts the thread's interpreter's execution loop.  
+`stop`: Stops the thread's interpreter's execution loop.  
+`pause`: Pauses the thread's interpreter's execution loop. It can be resumed with `start thread <thread>`. Not the same as the keyword `pause`.  
--- multisl.js
+++ multisl.js
@@ -1,5 +1,5 @@
-import { ISLInterpreter, ISLError } from "https://cdn.jsdelivr.net/gh/LightningLaser8/ISL@main/core/interpreter.js";
-import { ISLExtension } from "https://cdn.jsdelivr.net/gh/LightningLaser8/ISL@main/core/extensions.js";
+import { ISLInterpreter, ISLError } from "https://cdn.jsdelivr.net/gh/LightningLaser8/ISL@latest/core/interpreter.js";
+import { ISLExtension } from "https://cdn.jsdelivr.net/gh/LightningLaser8/ISL@latest/core/extensions.js";
 class MultISLExtension extends ISLExtension {
   threads = {};
   options = {};
@@ -18,63 +18,70 @@ class MultISLExtension extends ISLExtension {
 
     this.addKeyword(
       "thread",
-      function (interpreter, labels, threadName, ...isl) {
-        if (labels.length !== 1) {
-          throw new ISLError(
-            "Cannot perform multiple thread operations at once, please only use one label at a time.", SyntaxError
-          );
-        }
-        if (labels.includes("create")) {
-          this.threads[threadName] = {
-            name: threadName,
-            isl: [],
-            interpreter: new ISLInterpreter(this.getThreadOpts(threadName)),
-          };
-        } else {
-          if (!this.threads[threadName]) {
-            throw new ISLError(
-              "Cannot " +
-                labels[0] +
-                " a nonexistent thread '" +
-                threadName +
-                "'", ReferenceError
-            );
-          }
-        }
-        if (labels.includes("addto")) {
-          this.threads[threadName].isl.push(
-            isl.map((x) => '"' + x + '"').join(" ")
-          );
-        }
-        if (labels.includes("in")) {
-          this.threads[threadName].interpreter.executeLine(
-            isl.map((x) => '"' + x + '"').join(" ")
-          );
-        }
-        if (labels.includes("start")) {
-          this.threads[threadName].interpreter.loadISL(
-            this.threads[threadName].isl
-          );
-          this.threads[threadName].interpreter.startExecution();
-        }
-        if (labels.includes("stop")) {
-          this.threads[threadName].interpreter.stopExecution();
-        }
-        if (labels.includes("pause")) {
-          this.threads[threadName].interpreter.pauseExecution();
-        }
-        if (labels.includes("delete")) {
-          this.threads[threadName].interpreter.stopExecution();
-          delete this.threads[threadName];
-        }
-      }
+      (interpreter, labels, threadName, ...isl) => {
+        this.#isl_thread(labels, threadName.value, ...isl.map(x => x.value))
+      },
+      [
+        {type: "identifier", name: "thread"},
+        {type: "any", name: "code", optional: true, recurring: true}
+      ]
     );
   }
-  getThreadOpts(threadName) {
+  #isl_thread(labels, threadName, ...isl) {
+    if (labels.length !== 1) {
+      throw new ISLError(
+        "Cannot perform multiple thread operations at once, please only use one label at a time.", SyntaxError
+      );
+    }
+    if (labels.includes("create")) {
+      this.threads[threadName] = {
+        name: threadName,
+        isl: [],
+        interpreter: new ISLInterpreter(this.#getThreadOpts(threadName)),
+      };
+    } else {
+      if (!this.threads[threadName]) {
+        throw new ISLError(
+          "Cannot " +
+            labels[0] +
+            " a nonexistent thread '" +
+            threadName +
+            "'", ReferenceError
+        );
+      }
+    }
+    if (labels.includes("add-to")) {
+      this.threads[threadName].isl.push(
+        isl.map((x) => '"' + x + '"').join(" ")
+      );
+    }
+    if (labels.includes("in")) {
+      this.threads[threadName].interpreter.executeLine(
+        isl.map((x) => '"' + x + '"').join(" ")
+      );
+    }
+    if (labels.includes("start")) {
+      this.threads[threadName].interpreter.loadISL(
+        this.threads[threadName].isl
+      );
+      this.threads[threadName].interpreter.startExecution();
+    }
+    if (labels.includes("stop")) {
+      this.threads[threadName].interpreter.stopExecution();
+    }
+    if (labels.includes("pause")) {
+      this.threads[threadName].interpreter.pauseExecution();
+    }
+    if (labels.includes("delete")) {
+      this.threads[threadName].interpreter.stopExecution();
+      delete this.threads[threadName];
+    }
+  }
+  #getThreadOpts(threadName) {
     let options = structuredClone(this.options);
     options.name = this.options.name + " (thread '" + threadName + "')";
     return options;
   }
 }
 
-export default MultISLExtension;
+export { MultISLExtension };
